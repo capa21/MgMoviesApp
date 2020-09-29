c36a57713525e971010274e6a87058953109438a
Jesús Cabeza Acero <jesus.cabeza@ceiba.com.co>
2020-09-28 19:07:48 -0500

    Adicionando pruebas al componente top5-movies-item

--- src/app/top5movies/components/top5-movies-item/top5-movies-item.component.spec.ts
+++ src/app/top5movies/components/top5-movies-item/top5-movies-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { Top5MoviesItemComponent } from './top5-movies-item.component';
 
@@ -16,17 +17,29 @@ describe('Top5MoviesItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(Top5MoviesItemComponent);
     component = fixture.componentInstance;
+    component.movie = {
+      title: 'Home Alone 2',
+      release: '11/20/1992',
+      description: 'One year after Kevin McCallister was left home alone and had to defeat a pair of bumbling burglars, he accidentally finds himself stranded in New York City - and the same criminals are not far behind.',
+      image: 'https://m.media-amazon.com/images/M/MV5BNDI1MzM0Y2YtYmIyMS00ODE3LTlhZjEtZTUyNmEzMTNhZWU5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    component.movie = {
-      "title": "Home Alone 2",
-      "release": "11/20/1992",
-      "description": "One year after Kevin McCallister was left home alone and had to defeat a pair of bumbling burglars, he accidentally finds himself stranded in New York City - and the same criminals are not far behind.",
-      "image": "https://m.media-amazon.com/images/M/MV5BNDI1MzM0Y2YtYmIyMS00ODE3LTlhZjEtZTUyNmEzMTNhZWU5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"
-    }
-    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should the title be "Home Alone 2"', () => {
+    expect(component.movie.title).toEqual('Home Alone 2');
+  });
+
+  it('should the title be "Home Alone 2" in template', () => {
+    //Arrange
+    const de = fixture.debugElement.query(By.css('.aside__title'));
+    const el = de.nativeElement;
+    //Assert
+    expect(el.textContent.trim()).toEqual('Home Alone 2');
+  });
+
 });
